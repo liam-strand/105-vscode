918cfbcdf681417c9dd6b0c6140810bb439ce16f
liam-strand <Liam.Strand@tufts.edu>
2023-01-30 21:50:52 -0500

    remove bloat

--- client/src/configuration.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-
-export const configuration: vscode.LanguageConfiguration = {
-    wordPattern: /[\w\-.:<>*][\w\d.\\/\-?<>*!]+/,
-    indentationRules: {
-        decreaseIndentPattern: undefined,
-        increaseIndentPattern: /^\s*\(.*[^)]\s*$/
-    }
-}
--- client/src/extension.ts
+++ client/src/extension.ts
@@ -11,12 +11,11 @@
  */
 
 import * as vscode from 'vscode';
-import {configuration} from './configuration';
 
 export function activate(ctx: vscode.ExtensionContext) {
-    // ctx.subscriptions.push(vscode.languages.setLanguageConfiguration('scheme', configuration));
+    //
 }
 
 export function deactivate() {
-    // 
+    //
 }
